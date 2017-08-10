b7c6c503bbba1a5682d3e12c017d4bcab1f94c98
Andrei Shenets <andrei.shenets@itechart-group.com>
2017-08-10 11:29:04 +0300

    Added scanning functionality

--- src/Mp3TagsEncodingFixer/MainWindow.xaml.cs
+++ src/Mp3TagsEncodingFixer/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 using href.Utils;
+using File = TagLib.File;
 
 namespace Mp3TagsEncodingFixer
 {
@@ -13,7 +18,9 @@ namespace Mp3TagsEncodingFixer
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public string SelectedFolder { get; private set; }
+        public string SelectedFolder { get; set; }
+
+        public IEnumerable<Mp3> Mp3s { get; private set; }
 
 
         public MainWindow()
@@ -38,9 +45,120 @@ namespace Mp3TagsEncodingFixer
             }
         }
 
-        private void ScanFolder(object sender, RoutedEventArgs e)
+        // ReSharper disable once InconsistentNaming
+        private async void ScanFolderForMp3sWithCorruptedTags(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            string folder = SelectedFolder;
+
+            ScanFolderButton.IsEnabled = false;
+            try
+            {
+                if (Directory.Exists(folder))
+                {
+                    using (StreamWriter stringWriter = new StreamWriter("Scan.log", false, Encoding.UTF8))
+                    {
+                        Mp3s = await Task<List<Mp3>>.Factory.StartNew(() => GetMp3sWithCorruptedTags(stringWriter, folder));
+                        RaisePropertyChanged(nameof(Mp3s));
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                System.Windows.MessageBox.Show(this, exception.ToString());
+            }
+            finally
+            {
+                ScanFolderButton.IsEnabled = true;
+            }
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private List<Mp3> GetMp3sWithCorruptedTags(TextWriter errors, string folder)
+        {
+            IEnumerable<string> fileNames = Directory.EnumerateFiles(folder, "*.mp3", SearchOption.AllDirectories);
+
+            List<Mp3> result = new List<Mp3>();
+
+            foreach (string fileName in fileNames)
+            {
+                try
+                {
+                    using (File file = File.Create(fileName))
+                    {
+                        file.Mode = File.AccessMode.Read;
+
+                        Mp3 mp3 = GetMp3IfTagIsCorrupted(fileName, nameof(file.Tag.Album), file.Tag.Album);
+                        if (mp3 != null)
+                        {
+                            result.Add(mp3);
+                        }
+
+                        //bool result = FillPerformers(file.Tag.Performers, track);
+                        //result = FillAlbumName(file.Tag.Album, track) || result;
+                        //result = FillAlbumPerformers(file.Tag.AlbumArtists, track) || result;
+                        //result = FillTitle(file.Tag.Title, track) || result;
+                        //result = FillGenres(file.Tag.Genres, track) || result;
+                        //result = FillTrackNumber(file.Tag.Track, track) || result;
+                        //result = FillTrackCount(file.Tag.TrackCount, track) || result;
+                        //result = FillDiskNumber(file.Tag.Disc, track) || result;
+                        //result = FillDiskCount(file.Tag.DiscCount, track) || result;
+                        //result = FillComposers(file.Tag.Composers, track) || result;
+                        //result = FillYear(file.Tag.Year, track) || result;
+                        //result = FillSize(track) || result;
+                    }
+                }
+                catch (Exception e)
+                {
+                    errors.Write(fileName);
+                    errors.WriteLine(":");
+                    errors.WriteLine(e.ToString());
+                }
+            }
+
+            return result;
+        }
+
+        private static Mp3 GetMp3IfTagIsCorrupted(string fileName, string tagName, params string[] tagValue)
+        {
+            (bool isCorrupted, string[] fixedValue) = IsCorrupted(tagValue);
+            if (isCorrupted)
+            {
+                return new Mp3
+                    {
+                        FileName = fileName,
+                        TagName = tagName,
+                        TagValue = string.Join(", ", tagValue),
+                        TagValueFix = string.Join(", ", fixedValue)
+                    };
+            }
+
+            return null;
+        }
+
+        private static (bool isCorrupted, string[] fixedValue) IsCorrupted(params string[] strings)
+        {
+            string[] fixedValue = strings
+                .Where(item => item != null)
+                .Select(CorrectStringEncoding)
+                .ToArray();
+
+            bool isCorrupted = false;
+
+            if (fixedValue.Any())
+            {
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(strings[i])
+                        && !string.IsNullOrWhiteSpace(fixedValue[i])
+                        && !string.Equals(strings[i], fixedValue[i]))
+                    {
+                        isCorrupted = true;
+                        break;
+                    }
+                }
+            }
+
+            return (isCorrupted, fixedValue);
         }
 
         private void ApplySelectedFixes(object sender, RoutedEventArgs e)
@@ -49,7 +167,7 @@ namespace Mp3TagsEncodingFixer
         }
 
 
-        private string CorrectStringEncoding(string baseString)
+        private static string CorrectStringEncoding(string baseString)
         {
             Encoding[] outgoingEncodings = EncodingTools.DetectOutgoingEncodings(baseString);
 
--- src/Mp3TagsEncodingFixer/Mp3.cs
+++ src/Mp3TagsEncodingFixer/Mp3.cs
@@ -3,6 +3,7 @@
     public class Mp3
     {
         public bool IsSelected { get; set; }
+        public string FileName { get; set; }
         public string TagName { get; set; }
         public string TagValue { get; set; }
         public string TagValueFix { get; set; }
